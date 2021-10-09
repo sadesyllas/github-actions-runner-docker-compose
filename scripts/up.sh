35ed9a1da95791d77f5833713c488bd539b442a1
Spyridon Alfredos Desyllas <sadesyllas@gmail.com>
2021-10-09 16:07:20 +0300

    Give access to host's Docker socket

--- scripts/up.sh
+++ scripts/up.sh
@@ -2,6 +2,10 @@
 
 pushd "$(dirname "$0")/.."
 
-USER_ID=$(id -u) GROUP_ID=$(id -g) docker-compose up "$@" --remove-orphans
+\
+  USER_ID=$(id -u) \
+  GROUP_ID=$(id -g) \
+  DOCKER_GROUP_ID=$(ls -l -n /var/run/docker.sock | awk '{print $4}') \
+  docker-compose up "$@" --remove-orphans
 
 popd
