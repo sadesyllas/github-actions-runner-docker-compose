d073b446cdcfeedfb34faedfdb740176071c39fd
Spyridon Alfredos Desyllas <sadesyllas@gmail.com>
2021-10-12 19:55:30 +0300

    Install extra packages through the `PACKAGES` environment variable

--- scripts/up.sh
+++ scripts/up.sh
@@ -6,6 +6,7 @@ pushd "$(dirname "$0")/.."
   USER_ID=$(id -u) \
   GROUP_ID=$(id -g) \
   DOCKER_GROUP_ID=$(ls -l -n /var/run/docker.sock | awk '{print $4}') \
+  PACKAGES=xvfb \
   docker-compose up "$@" --remove-orphans
 
 popd
